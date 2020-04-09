d4ce23326da8dd74ed435c9e54abd2081c4d9196
Zero Qu <zhidongqu@gmail.com>
2020-04-08 20:18:54 -0700

    debug lastfm

--- src/apis/lastfm.js
+++ src/apis/lastfm.js
@@ -4,7 +4,7 @@ const KEY = process.env.REACT_APP_LAST_FM_KEY;
 
 export default axios.create(
     {
-        baseURL: `http://ws.audioscrobbler.com/2.0/?method=album.search&api_key=${KEY}&format=json&limit=1`,
+        baseURL: `https://ws.audioscrobbler.com/2.0/?method=album.search&api_key=${KEY}&format=json&limit=1`,
         timeout: 50000,
         headers: {
             'Content-Type': 'application/json',
