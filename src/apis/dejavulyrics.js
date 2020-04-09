a837e95776bd303550e324de23e534f56a4f58ff
Zero Qu <zhidongqu@gmail.com>
2020-04-08 18:10:35 -0700

    commit

--- src/apis/dejavulyrics.js
+++ src/apis/dejavulyrics.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://dejavulyrics-server.herokuapp.com',
     timeout: 50000,
     headers: {
         'Content-Type': 'application/json',
